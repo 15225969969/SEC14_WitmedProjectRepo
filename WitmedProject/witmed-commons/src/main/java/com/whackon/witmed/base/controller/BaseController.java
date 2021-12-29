eef8aab0725350bfdfc639724536fcb365dabb53
user <2427705096@qq.com>
2021-12-29 22:52:37 +0800

    创建并完成 Witmed Registry 模块

---
+++ WitmedProject/witmed-commons/src/main/java/com/whackon/witmed/base/controller/BaseController.java
@@ -0,0 +1,12 @@
+package com.whackon.witmed.base.controller;
+
+/**
+ * <b>基础信息功能-基础控制层类</b>
+ *
+ * @author user
+ * @date 2021/12/29
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class BaseController {
+}
---
+++ WitmedProject/witmed-commons/src/main/java/com/whackon/witmed/base/pojo/vo/BaseVO.java
@@ -0,0 +1,11 @@
+package com.whackon.witmed.base.pojo.vo;
+
+/**
+ * <b></b>
+ *
+ * @author user
+ * @date 2021/12/29
+ * @since
+ */
+public class BaseVO {
+}
